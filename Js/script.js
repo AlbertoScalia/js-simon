b1fa74a0c485daf21763fd9a5eb143b2efae6fe6
AlbertoScalia <albscalia@gmail.com>
2023-04-28 16:19:52 +0200

    Feat (JS) : Add second function

--- Js/script.js
+++ Js/script.js
@@ -3,4 +3,35 @@ function randomNumberGenerator(max, min) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+// Creo le variabili
+let play = document.getElementById("play");
+let listaNumeri = document.getElementById("lista_numeri");
+let numeriRandom;
 
+// Funzione che mi genera l'array
+function clickPlay() {
+    numeriRandom = [];
+    //Nel ciclo while inserisco cinque numeri dal generatore randomico
+    while (numeriRandom.length < 5) {
+        let num = randomNumberGenerator(100, 1);
+        if (!numeriRandom.includes(num)) {
+            numeriRandom.push(num)
+        }
+    }
+
+    //Porto i cinque numeri all'interno del DOM
+    let indice = 0
+    let intervallo = setTimeout(function () {
+        if (indice < numeriRandom.length) {
+            listaNumeri.innerHTML =
+                `
+                <h3 class="numeriRandom">${numeriRandom}</h3>
+            `
+        } else {
+            listaNumeri.innerHTML = ` `
+        }
+        indice++
+    }, 1000)
+
+    console.log(numeriRandom)
+}
